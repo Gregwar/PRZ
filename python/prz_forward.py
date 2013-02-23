ba0c35a2a43eeb93503e7eb84e408f2a12872444
Gregwar <g.passault@gmail.com>
2013-02-23 14:50:19 +0100

    prz

--- python/prz_forward.py
+++ python/prz_forward.py
@@ -7,8 +7,8 @@ target = ('192.168.0.22', 2525)
 port = 6454  # where do you expect to get a msg?
 bufferSize = 1024 # whatever you need
 
-o = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-o.bind(target)
+o = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+o.connect(target)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -20,5 +20,15 @@ print('Listening...')
 while True:
     result = select.select([s],[],[])
     msg = result[0][0].recv(bufferSize) 
+    arr = []
+    for x in xrange(len(msg)):
+        arr += [ord(msg[x])]
+    print(arr)
     if ord(msg[14]) == 1:
-        o.send(msg)
+        core = msg[18:]
+        packet = b'\xaa'
+        length = len(core)
+        packet += chr((length>>8) & 0xff)
+        packet += chr(length & 0xff)
+        packet += core
+        o.send(packet)
--- python/prz_server.py
+++ python/prz_server.py
@@ -20,7 +20,7 @@ def SerialThread(device):
                         packet += chr(buf[x])
 
                     #print(ord(packet[3]))
-                    print(length)
+                    #print('Sending '+str(length))
                     port.write(packet)
 
                 time.sleep(1.0/100)
@@ -47,12 +47,34 @@ def HandleData(data):
     globals()['buffer'] = target
 
 def ClientLoop(s):
-    while True:
+    dead = False
+    state = 0
+    l = 0
+    data = []
+    while not dead:
         c = s.recv(1024)
-        arr = []
-        for x in xrange(len(c)):
-            arr += [ord(c[x])]
-        HandleData(arr)
+        if not c:
+            dead = True
+        else:
+            for x in xrange(len(c)):
+                h = c[x]
+                if state == 3:
+                    data += [ord(h)]
+                    l = l-1
+                    if l <= 0:
+                        HandleData(data)
+                        state = 0
+                else:
+                    if state == 2:
+                        l |= ord(h)
+                        print('Len=' + str(l))
+                        data = []
+                        state = 3
+                    if state == 1:
+                        l = ord(h)<<8
+                        state = 2
+                    if state == 0 and h == b'\xaa':
+                        state = 1                    
 
 def ServerLoop():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,7 +102,7 @@ def main():
     sys.exit(2)
 
   universe = 1
-  device = '/dev/ttyUSB0'
+  device = '/dev/ttyUSB1'
   for o, a in opts:
      if o in ('-h', '--help'):
           Usage()
